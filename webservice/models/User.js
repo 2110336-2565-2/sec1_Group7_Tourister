fbd8c4521ee27ab40b41e545565547045a0b5dd0
teeradon21 <teeradon21@outlook.com>
2023-02-02 22:50:29 +0700

    Init user model

---
+++ webservice/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    citizenId: {
+        type: String,
+        required: [true,'Please add Citizen ID'],
+        unique: true,
+        maxlength:[13,'Citizen ID can not be more than 13 digits']
+    },
+    name: {
+        type: String,
+        trim:true,
+        required: [true, 'Please add a name']
+    },
+    surname: {
+        type: String,
+        trim:true,
+        required: [true, 'Please add a surname']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Please add an email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password']
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, 'Please add a phone number']
+    },
+    isGuide: {
+        type: Boolean
+    },
+    bankName: {
+        type: String
+    },
+    bankAccount: {
+        type: String
+    },
+    licenseId: {
+        type: String,
+        unique: true
+    }
+});
+
+module.exports=mongoose.model('User',UserSchema);
