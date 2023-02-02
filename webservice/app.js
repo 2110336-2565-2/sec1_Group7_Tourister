a2601c4590d0444a7b62d3fcbfd35c8636b181e7
bbompk <panithiv@gmail.com>
2023-02-02 14:04:00 +0700

    setup backend project directories

---
+++ webservice/app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const logger = require('morgan')
+
+const app = express()
+
+//connect mongodb atlas
+const mongo_uri = "mongodb+srv://root:tourist7@tourister.zr7ni4z.mongodb.net/test";
+mongoose.Promise = global.Promise;
+mongoose.connect(mongo_uri, { useNewUrlParser : true});
+
+//mongodb error handler
+mongoose.connection.on('error', err => 
+    {
+        console.error('MongoDB error', err);
+    }
+)
+
+
+// logger middleware
+app.use(logger('dev'))
+
+app.get('/', async (req, res, next) => {
+    console.log("Hello SE2 Project !!")
+    return res.json({ message : "Hello World"})
+})
+
+
+// Error handler
+/**
+   * errorHandler
+   * @param {import('express').Request} req 
+   * @param {import('express').Response} res 
+   * @param {import('express').NextFunction} next 
+   */
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err?.code || err?.status || 500;
+  
+    return res.status(statusCode).json({
+      code: statusCode,
+      message: err?.message || 'Error'
+    });
+}
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => 
+    {
+        console.log(`Server is running on PORT ${PORT}`);
+    }
+)
