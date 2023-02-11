5283433e4383339aa19d8ad9d08e8d836b788a87
AunchisaS <creamaunchisa09@gmail.com>
2023-02-11 10:10:20 +0700

    create program button in trips-page

---
+++ frontend/app/trips/Landing.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from '@mui/material';
+import React from 'react';
+
+const Landing = () => {
+    
+    
+    return (
+
+        <Button  href="/trips/createTrip" variant="contained">
+            Create Trip
+        </Button>    
+    );
+}
+export default Landing;
