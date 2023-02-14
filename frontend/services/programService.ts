cfdf66ee6685fb51775c9f83679f1fdee835f6a3
bbompk <panithiv@gmail.com>
2023-02-14 13:53:17 +0700

    fix: front end axios response type in service

--- frontend/services/programService.ts
+++ frontend/services/programService.ts
@@ -7,31 +7,36 @@ import axios from "axios";
 
 export const getAllPrograms = async () => {
     const query = undefined // TODO: discuss how will we query data
-    const res = await axios.get(`${appConfig.BACKEND_URL}/api/program`) as ApiResponseInterface<ProgramInterface[]>
+    const axios_res = await axios.get(`${appConfig.BACKEND_URL}/api/program`)
+    const res = axios_res.data as ApiResponseInterface<ProgramInterface[]>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 } 
 
 export const getProgramById = async (id: number) => {
-    const res = await axios.get(`${appConfig.BACKEND_URL}/api/program/${id}`) as ApiResponseInterface<ProgramInterface>
+    const axios_res = await axios.get(`${appConfig.BACKEND_URL}/api/program/${id}`)
+    const res = axios_res.data as ApiResponseInterface<ProgramInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const createProgram = async (data: ProgramInterface) => {
-    const res = await axios.post(`${appConfig.BACKEND_URL}/api/program`, data) as ApiResponseInterface<ProgramInterface>
+    const axios_res = await axios.post(`${appConfig.BACKEND_URL}/api/program`, data) 
+    const res = axios_res.data as ApiResponseInterface<ProgramInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const updateProgramById = async (id: number, data: any) => {
-    const res = await axios.put(`${appConfig.BACKEND_URL}/api/program/${id}`, data) as ApiResponseInterface<ProgramInterface>
+    const axios_res = await axios.put(`${appConfig.BACKEND_URL}/api/program/${id}`, data) 
+    const res = axios_res.data as ApiResponseInterface<ProgramInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const deleteProgramById = async (id: number) => {
-    const res = await axios.delete(`${appConfig.BACKEND_URL}/api/program/${id}`) as ApiResponseInterface<ProgramInterface>
+    const axios_res = await axios.delete(`${appConfig.BACKEND_URL}/api/program/${id}`)
+    const res = axios_res.data as ApiResponseInterface<ProgramInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
--- frontend/services/userService.ts
+++ frontend/services/userService.ts
@@ -7,37 +7,43 @@ import axios from "axios";
 
 export const getAllUsers = async () => {
     const query = undefined // TODO: discuss how will we query data
-    const res = await axios.get(`${appConfig.BACKEND_URL}/api/user`) as ApiResponseInterface<UserInterface[]>
+    const axios_res = await axios.get(`${appConfig.BACKEND_URL}/api/user`) 
+    const res = axios_res.data as ApiResponseInterface<UserInterface[]>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 } 
 
 export const getUserById = async (id: number) => {
-    const res = await axios.get(`${appConfig.BACKEND_URL}/api/user/${id}`) as ApiResponseInterface<UserInterface>
+    const axios_res = await axios.get(`${appConfig.BACKEND_URL}/api/user/${id}`) 
+    const res = axios_res.data as ApiResponseInterface<UserInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const createUser = async (data: UserInterface) => {
-    const res = await axios.post(`${appConfig.BACKEND_URL}/api/user`, data) as ApiResponseInterface
+    const axios_res = await axios.post(`${appConfig.BACKEND_URL}/api/user`, data)
+    const res = axios_res.data as ApiResponseInterface<UserInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const registerUser = async (data: UserInterface) => {
-    const res = await axios.post(`${appConfig.BACKEND_URL}/api/user`, data) as ApiResponseInterface
+    const axios_res = await axios.post(`${appConfig.BACKEND_URL}/api/user`, data)
+    const res = axios_res.data as ApiResponseInterface<UserInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const updateUserById = async (id: number, data: any) => {
-    const res = await axios.put(`${appConfig.BACKEND_URL}/api/user/${id}`, data) as ApiResponseInterface<UserInterface>
+    const axios_res = await axios.put(`${appConfig.BACKEND_URL}/api/user/${id}`, data)
+    const res = axios_res.data as ApiResponseInterface<UserInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
 
 export const deleteUserById = async (id: number) => {
-    const res = await axios.delete(`${appConfig.BACKEND_URL}/api/user/${id}`) as ApiResponseInterface<UserInterface>
+    const axios_res = await axios.delete(`${appConfig.BACKEND_URL}/api/user/${id}`)
+    const res = axios_res.data as ApiResponseInterface<UserInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined)
     return res;
 }
