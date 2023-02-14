9986082ae6be9e75f231c55c1398ade8424a863a
Akkharawat <akkharawatbct@gmail.com>
2023-02-15 02:03:25 +0700

    can show cards in all status

--- frontend/components/request/navbar.tsx
+++ frontend/components/request/navbar.tsx
@@ -2,87 +2,20 @@
 
 import React from "react";
 import { useState, useEffect } from "react";
-import Accepted from "./accepted/page";
-import { ProgramInterface } from "@/interfaces/ProgramInterface";
-import { UserCardInterface } from "@/interfaces/UserCardInterface";
-import { UserInterface } from "@/interfaces/UserInterface";
-import { getUserById } from "@/services/userService";
 import axios from "axios";
-import Link from "next/link";
 import { Button, AppBar } from "@mui/material";
 
 const API_URL = `http://localhost:2000/api/program`;
-var data: any = [];
 
 export default function Navbar({ value, setValue }: any) {
   const [isClicked, setIsClicked] = useState(false);
   const [cardstatus, setcardStatus] = useState<string>();
-  const [userInfo, setuserInfo] = useState([
-    {
-      citizenId: "",
-      name: "",
-      surname: "",
-      email: "",
-      password: "",
-      phoneNumber: "",
-      isGuide: false,
-      bankName: "",
-      bankAccount: "",
-      remainingAmount: 0,
-      imageURL: "",
-    },
-  ]);
-  const [data, setData] = useState([
-    {
-      _id: "",
-      programId: "",
-      name: "",
-      description: "",
-      startDate: Date,
-      endDate: Date,
-      startTime: Date,
-      endTime: Date,
-      max_participant: 0,
-      num_participant: 0,
-      meetLocation: "",
-      descriptionOfMeetLocation: "",
-      attractions: [],
-      imageUrl: "",
-      language: "",
-      endLocation: "",
-      descriptionOfEndLocation: "",
-      pending_participant: [],
-      accepted_participant: [],
-      declined_participant: [],
-    },
-  ]);
-
-  const fillUser = async (userid: string) => {
-    // var arr: any = [];
-    // const res = await axios.get(`http://localhost:2000/api/user/${userid}`);
-    // console.log(res.data);
-    // setuserInfo(res.data.data);
-    // arr.push(res.data.name);
-    // arr.push(res.data.surname);
-    // arr.push(res.data.email);
-    // return res.data.name;
-    // return arr;
-  };
-
-  const handleChange = async () => {
-    const res = await axios.get(API_URL);
-    // console.log(res.data);
-    setData(res.data.data);
-  };
 
   const statusChange = async (status: string) => {
     setcardStatus(status);
     setIsClicked(!isClicked);
-    // console.log(cardstatus);
-    // handleChange();
   };
   console.log(cardstatus);
-  // console.log(data);
 
   const [programs, setPrograms] = useState([
     {
@@ -109,13 +42,48 @@ export default function Navbar({ value, setValue }: any) {
       user: [],
     },
   ]);
-  // useEffect(() => {
   if (isClicked && cardstatus === "accepted") {
     setIsClicked(false);
     axios.get(`http://localhost:2000/api/program`).then((response: any) => {
       // console.log(response.data);
       const programsWithUsers = response.data.data.map((program: any) => {
         const userPromises = program.accepted_participant.map((userId: any) =>
+          axios.get(`http://localhost:2000/api/user/${userId}`)
+        );
+        return Promise.all(userPromises).then((user) => ({
+          ...program,
+          user,
+        }));
+      });
+      Promise.all(programsWithUsers).then((programsWithUsers) => {
+        setPrograms(programsWithUsers);
+      });
+    });
+  }
+  if (isClicked && cardstatus === "pending") {
+    setIsClicked(false);
+    axios.get(`http://localhost:2000/api/program`).then((response: any) => {
+      // console.log(response.data);
+      const programsWithUsers = response.data.data.map((program: any) => {
+        const userPromises = program.pending_participant.map((userId: any) =>
+          axios.get(`http://localhost:2000/api/user/${userId}`)
+        );
+        return Promise.all(userPromises).then((user) => ({
+          ...program,
+          user,
+        }));
+      });
+      Promise.all(programsWithUsers).then((programsWithUsers) => {
+        setPrograms(programsWithUsers);
+      });
+    });
+  }
+  if (isClicked && cardstatus === "declined") {
+    setIsClicked(false);
+    axios.get(`http://localhost:2000/api/program`).then((response: any) => {
+      // console.log(response.data);
+      const programsWithUsers = response.data.data.map((program: any) => {
+        const userPromises = program.declined_participant.map((userId: any) =>
           axios.get(`http://localhost:2000/api/user/${userId}`)
         );
         return Promise.all(userPromises).then((user) => ({
@@ -147,21 +115,17 @@ export default function Navbar({ value, setValue }: any) {
             alignSelf: "center",
           }}
         >
-          {/* <Link href="./request/pending" passHref> */}
-          <button type="button" onClick={() => statusChange("pending")}>
+          <Button type="button" onClick={() => statusChange("pending")}>
             Pending
-          </button>
-          {/* </Link> */}
-          {/* <Link href="./request/accepted" passHref> */}
-          <button type="button" onClick={() => statusChange("accepted")}>
+          </Button>
+
+          <Button type="button" onClick={() => statusChange("accepted")}>
             Accepted
-          </button>
-          {/* </Link> */}
-          {/* <Link href="./request/declined" passHref> */}
-          <button type="button" onClick={() => statusChange("declined")}>
+          </Button>
+
+          <Button type="button" onClick={() => statusChange("declined")}>
             Declined
-          </button>
-          {/* </Link> */}
+          </Button>
         </div>
       </nav>
       <div>
@@ -169,18 +133,21 @@ export default function Navbar({ value, setValue }: any) {
           <div key={program._id}>
             <ul>
               {program.user.map((user: any) => (
-                <h3 key={user.data.data._id}>
+                <div key={user.data.data._id}>
                   {user.data.data.name} {user.data.data.surname}
                   <div>Contact: {user.data.data.email}</div>
-                </h3>
+                  <h2>{program.name}</h2>
+                  <p>{program.description}</p>
+                  <p>{program.startDate}</p>
+                  <p>
+                    {program.num_participant}/{program.max_participant}
+                  </p>
+                  <p>
+                    ---------------------------------------------------------
+                  </p>
+                </div>
               ))}
             </ul>
-            <h2>{program.name}</h2>
-            <p>{program.description}</p>
-            <p>{program.startDate}</p>
-            <p>
-              {program.num_participant}/{program.max_participant}
-            </p>
           </div>
         ))}
       </div>
