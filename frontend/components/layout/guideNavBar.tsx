c5303aab1f5877e68547af6858ef53bee70aab5e
Pras Pitasawad <prasrodo@gmail.com>
2023-02-15 14:26:01 +0700

    Improve UI of guide bottom navbar

--- frontend/components/layout/guideNavBar.tsx
+++ frontend/components/layout/guideNavBar.tsx
@@ -1,85 +1,55 @@
-import Link from 'next/link';
-import { css } from '@emotion/react';
+import { Paper, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function GuideNavBar() {
-  const pathname = usePathname();
-  console.log(pathname);
+  const router = useRouter();
+  const [path, setPath] = useState<String|null>(router.pathname)
 
-  const nav = css({
-    display: 'flex',
-    bottom: 0,
-    width: "100%",
-    position: "fixed",
-    boxShadow: "0 0 3px SportsRugbySharp(0,0,0,0.2)",
-    overflowX: "auto",
-    justifyContent: "space-around",
-    color: "gray",
-    fontFamily: "Avenir, Sans-serif",
-    fontWeight: "bold",
-    fontSize: "0.85rem",
-    left: "0px",
-    paddingBottom: "10px",
-    borderTop: "1px solid #202020",
-    BoxShadow: "0px, 1px, 1px, rgba(50, 50, 50, 0.75",
-    paddingTop: "10px"
-  });
-
-  const navLink = css({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    flexGrow: 1,
-    minWidth: '50px',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    color: 'gray',
-  });
-
-  const navLinkActive = css({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    flexGrow: 1,
-    minWidth: '50px',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    color: '#4682B4',
-  });
+  const handleChange = (event: any, newPathname:String) => {
+    setPath(newPathname)
+  }
+  
+  const onLink = (href: string) => {
+    router.push(href)
+  };
 
   return (
-    <section>
-      {/* Include shared UI here e.g. a header or sidebar */}
-      <nav css={nav}>
-        <Link href="/trips" style={{ textDecoration: 'none'}}>
-          <div css={navLink}>
-            <BusinessCenterOutlinedIcon />
-            Trips
-          </div>
-        </Link>
-        <Link href="/request" style={{ textDecoration: 'none'}}>
-            <div css={navLink}>
-              <CalendarMonthOutlinedIcon />
-              Request
-            </div>
-        </Link>
-        <Link href="/request" style={{ textDecoration: 'none'}}>
-          <div css={navLink}>
-            <NotificationsNoneOutlinedIcon />
-            Notification
-          </div>
-        </Link>
-        <Link href="/manage_account" style={{ textDecoration: 'none'}}>
-          <div css={navLink}>
-            <AccountCircleOutlinedIcon />
-            Account
-          </div>
-        </Link>
-      </nav>
-    </section>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, color:"light", zIndex:100}} elevation={5}>
+        <BottomNavigation
+          showLabels
+          value={path}
+          onChange={handleChange}
+          >
+          <BottomNavigationAction 
+            label="Trips" 
+            value="/trips" 
+            icon={<BusinessCenterOutlinedIcon fontSize="large"/>} 
+            onClick={()=>onLink("/trips")}
+            />
+          <BottomNavigationAction 
+            label="Request" 
+            value="/request" 
+            icon={<CalendarMonthOutlinedIcon fontSize="large"/>} 
+            onClick={()=>onLink("/request")}
+            />
+          <BottomNavigationAction 
+            label="Notication" 
+            value="/notification" 
+            icon={<NotificationsNoneOutlinedIcon fontSize="large"/>} 
+            onClick={()=>onLink("/trips")}
+          />
+          <BottomNavigationAction 
+            label="Account" 
+            value="/manage_account" 
+            icon={<AccountCircleOutlinedIcon fontSize="large"/>} 
+            onClick={()=>onLink("/manage_account")}
+            />
+      </BottomNavigation>
+    </Paper>
   );
 }
