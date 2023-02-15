08df1a9d60ff736ba5274da6b4a69eb949582cc6
Pras Pitasawad <prasrodo@gmail.com>
2023-02-15 15:58:46 +0700

    import poppins font to all pages

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
+import "@fontsource/poppins"
 import "@/css/global.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
