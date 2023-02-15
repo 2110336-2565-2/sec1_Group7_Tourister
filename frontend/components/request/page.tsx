5230fab2775ac21bd719b90bf4958859ac298102
Akkharawat <akkharawatbct@gmail.com>
2023-02-15 15:44:23 +0700

    remove card when guide accept/declined users

--- frontend/components/request/page.tsx
+++ frontend/components/request/page.tsx
@@ -10,64 +10,7 @@ import { getUserById } from "@/services/userService";
 export default function Navbar() {
   const [isClicked, setIsClicked] = useState(false);
   const [cardstatus, setcardStatus] = useState<string>();
-
-  const statusChange = async (status: string) => {
-    setcardStatus(status);
-    setIsClicked(!isClicked);
-  };
-  console.log(cardstatus);
-
-  const moveUsers = async (userid: any, programid: any, status: string) => {
-    if (status === "accepted") {
-      const res = await getProgramById(programid);
-      console.log(userid);
-      // console.log(programid);
-      const acceptedarr = res.data.accepted_participant;
-      acceptedarr.push(userid);
-      const pendingarr = res.data.pending_participant.filter(
-        (user: any) => user !== userid
-      );
-      console.log(pendingarr);
-      console.log(acceptedarr);
-
-      const update = {
-        $set: {
-          pending_participant: pendingarr,
-          accepted_participant: acceptedarr,
-        },
-      };
-
-      console.log(update);
-      console.log(programid);
-      const response = await updateProgramById(programid, update);
-      console.log(response.data);
-    }
-    if (status === "declined") {
-      const res = await getProgramById(programid);
-      console.log(userid);
-      // console.log(programid);
-      const declineddarr = res.data.declined_participant;
-      declineddarr.push(userid);
-      const pendingarr = res.data.pending_participant.filter(
-        (user: any) => user !== userid
-      );
-      console.log(pendingarr);
-      console.log(declineddarr);
-
-      const update = {
-        $set: {
-          pending_participant: pendingarr,
-          declined_participant: declineddarr,
-        },
-      };
-
-      console.log(update);
-      console.log(programid);
-      const response = await updateProgramById(programid, update);
-      console.log(response.data);
-    }
-  };
-
+  const [showCard, setShowCard] = useState(true);
   const [programs, setPrograms] = useState([
     {
       _id: "",
@@ -93,6 +36,62 @@ export default function Navbar() {
       user: [],
     },
   ]);
+
+  const statusChange = async (status: string) => {
+    setcardStatus(status);
+    setIsClicked(!isClicked);
+  };
+  console.log(cardstatus);
+
+  const moveUsers = async (userid: any, programid: any, status: string) => {
+    if (status === "accepted") {
+      const res = await getProgramById(programid);
+      // console.log(userid);
+      // console.log(programid);
+      const acceptedarr = res.data.accepted_participant;
+      acceptedarr.push(userid);
+      const pendingarr = res.data.pending_participant.filter(
+        (user: any) => user !== userid
+      );
+      // console.log(pendingarr);
+      // console.log(acceptedarr);
+
+      const update = {
+        pending_participant: pendingarr,
+        accepted_participant: acceptedarr,
+      };
+
+      // console.log(update);
+      // console.log(programid);
+      const response = await updateProgramById(programid, update);
+      // console.log(response.data);
+      setShowCard(false);
+    }
+    if (status === "declined") {
+      const res = await getProgramById(programid);
+      // console.log(userid);
+      // console.log(programid);
+      const declineddarr = res.data.declined_participant;
+      declineddarr.push(userid);
+      const pendingarr = res.data.pending_participant.filter(
+        (user: any) => user !== userid
+      );
+      // console.log(pendingarr);
+      // console.log(declineddarr);
+
+      const update = {
+        pending_participant: pendingarr,
+        declined_participant: declineddarr,
+      };
+
+      // console.log(update);
+      // console.log(programid);
+      const response = await updateProgramById(programid, update);
+      // console.log(response.data);
+      setShowCard(false);
+    }
+  };
+
   if (isClicked && cardstatus === "accepted") {
     setIsClicked(false);
     axios.get(`http://localhost:2000/api/program`).then((response: any) => {
@@ -149,7 +148,7 @@ export default function Navbar() {
   }
   // }, []);
   console.log(programs);
-  console.log(programs[0].user);
+  // console.log(programs[0].user);
 
   return (
     <div>
@@ -181,45 +180,46 @@ export default function Navbar() {
       </nav>
       {cardstatus === "pending" && (
         <div>
-          {programs.map((program) => (
-            <div key={program._id}>
-              <ul>
-                {program.user.map((user: any) => (
-                  <div key={user.data.data._id}>
-                    {user.data.data.name} {user.data.data.surname}
-                    <div>Contact: {user.data.data.email}</div>
-                    <h2>{program.name}</h2>
-                    <p>{program.description}</p>
-                    <p>{program.startDate}</p>
-                    <p>
-                      {program.num_participant}/{program.max_participant}
-                    </p>
-                    <Button
-                      type="button"
-                      variant="outlined"
-                      onClick={() =>
-                        moveUsers(user.data.data._id, program._id, "declined")
-                      }
-                    >
-                      DECLINED
-                    </Button>
-                    <Button
-                      type="button"
-                      variant="contained"
-                      onClick={() =>
-                        moveUsers(user.data.data._id, program._id, "accepted")
-                      }
-                    >
-                      ACCEPT
-                    </Button>
-                    <p>
-                      ---------------------------------------------------------
-                    </p>
-                  </div>
-                ))}
-              </ul>
-            </div>
-          ))}
+          {showCard &&
+            programs.map((program) => (
+              <div key={program._id}>
+                <ul>
+                  {program.user.map((user: any) => (
+                    <div key={user.data.data._id}>
+                      {user.data.data.name} {user.data.data.surname}
+                      <div>Contact: {user.data.data.email}</div>
+                      <h2>{program.name}</h2>
+                      <p>{program.description}</p>
+                      <p>{program.startDate}</p>
+                      <p>
+                        {program.num_participant}/{program.max_participant}
+                      </p>
+                      <Button
+                        type="button"
+                        variant="outlined"
+                        onClick={() =>
+                          moveUsers(user.data.data._id, program._id, "declined")
+                        }
+                      >
+                        DECLINED
+                      </Button>
+                      <Button
+                        type="button"
+                        variant="contained"
+                        onClick={() =>
+                          moveUsers(user.data.data._id, program._id, "accepted")
+                        }
+                      >
+                        ACCEPT
+                      </Button>
+                      <p>
+                        ---------------------------------------------------------
+                      </p>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            ))}
         </div>
       )}
       {(cardstatus === "accepted" || cardstatus === "declined") && (
