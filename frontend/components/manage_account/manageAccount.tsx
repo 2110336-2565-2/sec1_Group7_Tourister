3cc9aa3f68ded154c7d82b5148a72eedf79e0e77
AkiraSitdhi <ppoonpy@gmail.com>
2023-02-22 14:17:24 +0700

    test styling manage_account

--- frontend/components/manage_account/manageAccount.tsx
+++ frontend/components/manage_account/manageAccount.tsx
@@ -1,7 +1,25 @@
 import { useState, MouseEvent } from "react";
 import Link from 'next/link';
+import { COLOR } from "@/theme/globalTheme";
+import styled from "styled-components";
 
 type AccountType = 'tourist' | 'guide';
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  
+  
+  width: 100%;
+  background: transparent;
+  padding: 1.5rem;
+`
+
+const Icon = styled.div`
+  background: red;
+`
 
 const manageAccount = () => {
   const [name, setName] = useState('Admin');
@@ -26,8 +44,31 @@ const manageAccount = () => {
 
   return (
     <form style={{display:'flex', alignItems: 'center',flexDirection:'column'}}onSubmit={handleSubmit}>
-      <Link href="./manage_account/editProfile" passHref><button type="button">Edit Profile</button></Link>
-      <Link href="../register" passHref><button type="button">Transfer Funds</button></Link>
+      {/* <ButtonContainer>
+        <div>Test1</div>
+        <div>Test2</div>
+        <div>Test3</div>
+      </ButtonContainer> */}
+      
+      <ButtonContainer>
+        <Icon>???</Icon>
+        <Link href="./manage_account/editProfile" style={{width:"60%"}} passHref><button type="button">Edit Profile</button></Link>
+        <Icon style={{width:"20%"}}>???</Icon>
+      </ButtonContainer>
+      <ButtonContainer>
+        <Icon style={{width:"20%"}}>???</Icon>
+        <Link href="" style={{width:"60%"}} passHref><button type="button">Transfer Funds</button></Link>
+        <Icon style={{width:"20%"}}>???</Icon>
+      </ButtonContainer>
+      <ButtonContainer>
+        <Icon style={{width:"20%"}}>???</Icon>
+        <Link href="" style={{width:"60%"}} passHref><button type="button">Log Out</button></Link>
+        <Icon style={{width:"20%"}}>???</Icon>
+      </ButtonContainer>
+
+
+      {/* <Link href="./manage_account/editProfile" passHref><button type="button">Edit Profile</button></Link>
+      <Link href="../register" passHref><button type="button">Transfer Funds</button></Link> */}
     </form>
   );
 };
