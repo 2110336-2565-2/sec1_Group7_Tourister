73203ba6cfeea00dd4d6af5785cf3cfb46683a38
Bhuribhat <Bhuribhat@gmail.com>
2023-02-23 12:09:04 +0700

    fix field styling in register

--- frontend/components/register/registerForm.tsx
+++ frontend/components/register/registerForm.tsx
@@ -11,7 +11,7 @@ import { FormInputHiddenText } from "@/components/formInput/FormInputHiddenText"
 import { COLOR } from "@/theme/globalTheme";
 import { PrimaryButton, RequireFormLabel } from "@/css/styling";
 import { FormInputAccountType } from "../formInput/FormInputAccountType";
-import { Form, FieldName } from "@/css/layout";
+import { Form, FieldName, Field } from "@/css/layout";
 
 
 const registerForm = () => {
@@ -63,28 +63,46 @@ const registerForm = () => {
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
+      <Field>
       <FieldName>Choose Account Type</FieldName>
       <FormInputAccountType name="accountType" control={control} label="" options={[{label: "tourist", value: "tourist"}, { label:"guide", value: "guide" }]}/>
+      </Field>
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Name</RequireFormLabel>
       <FormInputText name="name" control={control} label="Name"/>
+      </Field>
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Surname</RequireFormLabel>
       <FormInputText name="surname" control={control} label="Surname"/>
+      </Field>
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Citizen ID</RequireFormLabel>
       <FormInputText name="citizenId" control={control} label="Citizen ID"/>
+      </Field>
       {watchAccountType==="guide" && 
         <>
+          <Field>
           <RequireFormLabel className="AsteriskRequired">Guide License ID</RequireFormLabel>
           <FormInputText name="guideLicenseId" control={control} label="License ID"/>
+          </Field>
         </>
       }
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Phone number</RequireFormLabel>
       <FormInputText name="phoneNumber" control={control} label="Phone number"/>
+      </Field>
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Email</RequireFormLabel>
       <FormInputText name="email" control={control} label="Email"/>
+      </Field>
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Password</RequireFormLabel>
       <FormInputHiddenText name="password" control={control} label="Password" showPassword={showPassword} handleClickShowPassword={handleClickShowPassword} handleMouseDownPassword={handleMouseDownPassword} />
+      </Field>
+      <Field>
       <RequireFormLabel className="AsteriskRequired">Confirm Password</RequireFormLabel>
       <FormInputHiddenText name="confirmPassword" control={control} label="Confirm Password" showPassword={showConfirmPassword} handleClickShowPassword={handleClickShowConfirmPassword} handleMouseDownPassword={handleMouseDownPassword} />
+      </Field>
       <PrimaryButton style={{alignSelf:"center"}} type="submit" variant="contained" >Sign Up </PrimaryButton>
       <div style={{alignSelf:"center", fontSize:"0.8rem", marginTop:"0.5rem"}}>
         <label>Already have an account? </label>
