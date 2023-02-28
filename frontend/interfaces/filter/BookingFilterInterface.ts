5658d806b7d72cc28ac9f07068fa5a19b721962c
bbompk <panithiv@gmail.com>
2023-02-28 10:21:26 +0700

    fix: correct BookingFilterInterface

--- frontend/interfaces/filter/BookingFilterInterface.ts
+++ frontend/interfaces/filter/BookingFilterInterface.ts
@@ -1,5 +1,5 @@
 import { BookingStatusInterface } from "../BookingInterface";
 
 export interface BookingFilterInterface {
-    status?: BookingFilterInterface | BookingFilterInterface[],
+    status?: BookingStatusInterface | BookingStatusInterface[],
 }
