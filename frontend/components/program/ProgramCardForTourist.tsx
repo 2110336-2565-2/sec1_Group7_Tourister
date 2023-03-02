39c1c847929920c3d7fbe137726ac175a0b83a88
Chanawatv <nanae11879@gmail.com>
2023-03-02 15:24:42 +0700

    add mock image for program and guide

--- frontend/components/program/ProgramCardForTourist.tsx
+++ frontend/components/program/ProgramCardForTourist.tsx
@@ -27,20 +27,21 @@ export const ProgramCardForTourist:FC<IProgramInterface> = ({program}) => {
   const endDate = new Date(program.endDate)
   return <> 
   <CardContainer>
-    <div style={{ alignSelf:"flex-end", color:"gray", display:"flex", justifyContent:"center", alignItems:"center" }}>
+    <div style={{ alignSelf:"flex-end", color:"gray", display:"flex", justifyContent:"center", alignItems:"center", marginBottom: "0.5rem"}}>
       <GroupOutlined/>
       <label style={{ fontSize: "1rem", padding: "0 0.6rem" }}> {program.num_participant}/{program.max_participant} </label>
     </div>
-    {/* <CardMedia 
-      image= {program.image} // TODO: add program image
-    /> */}
+    <CardMedia 
+      image= {program.image || "https://t3.ftcdn.net/jpg/01/32/94/50/240_F_132945016_nfxSTTLLFGlgxb35E7kTn09oQ3bvZqeD.jpg"} // TODO: add program image
+      style={{ height: "40vw", width: "100%", objectFit: "cover", borderRadius: "15px"}}
+    />
     <h4 style={{ margin:"0.3rem 0", wordWrap:"break-word" }}>{program.name}</h4>
     <LocationTag location={program.province} />
     <label style={{ margin:"0.5rem 0", wordWrap:"break-word", color:"gray", fontSize:"0.8rem" }}>{program.description}</label>
     <div style={{ display:"flex", flexDirection:"row", justifyContent:"center", alignItems:"center" }}>
-      <Image
-          src={tourist} // TODO: add tourist profile image
-          alt = '' width={22} height={22} 
+      <CardMedia 
+        image= {/* {guide image here} || */ "https://cdn-icons-png.flaticon.com/512/7723/7723210.png"} // TODO: add guide profile image
+        style={{ height: "22px", width: "22px"}}
       />
       <label style={{color: "grey", fontSize: "0.7rem", paddingLeft: "0.6rem"}}>{program.guide.name} {program.guide.surname}</label>
       <div style={{marginLeft:"auto", paddingRight: "0.6rem"}}>THB <label style={{color: COLOR.primary}}>{program.price}</label></div>
