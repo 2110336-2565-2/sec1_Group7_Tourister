92523dc4889cbe26d58759a32dbfc44610671f47
bbompk <panithiv@gmail.com>
2023-03-12 20:27:23 +0700

    fix: set express body parser limit

--- webservice/app.js
+++ webservice/app.js
@@ -11,7 +11,7 @@ const authRoute = require('./routes/auth')
 
 const app = express()
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'))
 app.use(cors())
