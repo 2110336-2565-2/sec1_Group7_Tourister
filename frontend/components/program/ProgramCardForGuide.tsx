595a2265cb6d4e3e228e14bb0451f4f2be4fa933
Bhuribhat <Bhuribhat@gmail.com>
2023-03-28 09:05:07 +0700

    adjust padding programcard for guide

--- frontend/components/program/ProgramCardForGuide.tsx
+++ frontend/components/program/ProgramCardForGuide.tsx
@@ -68,7 +68,8 @@ export const ProgramCardForGuide: FC<IProgramInterface> = ({
                 border: "1px solid grey",
                 borderRadius: 10,
                 textAlign: "center",
-                letterSpacing: "1.5px"
+                letterSpacing: "1.5px",
+                transform: "translateX(-10px)"
               }}
             >
               {program.num_participant}/{program.max_participant}
@@ -92,7 +93,11 @@ export const ProgramCardForGuide: FC<IProgramInterface> = ({
       <div
         key={program._id}
         onClick={isDraft?()=>{}:handleClick}
-        style={{ borderBottom: `2px solid ${COLOR.paleblue}`, padding: "1em 1em 0.25em 1em " }}
+        style={{ 
+          height: "180px",
+          borderBottom: `2px solid ${COLOR.paleblue}`, 
+          padding: "1em 1em 0.25em 1em" 
+        }}
       >
         <div>
           {isDraft? (<button type="button" onClick={handleFunction}><DriveFileRenameOutlineIcon/></button>):(<Fragment/>)}
