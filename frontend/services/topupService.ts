3b63cae8cee669e5f58d8d499a77cc1ead385408
bbompk <panithiv@gmail.com>
2023-04-02 01:03:59 +0700

    fix: correct topup route in frontend

--- frontend/services/topupService.ts
+++ frontend/services/topupService.ts
@@ -8,7 +8,7 @@ import { UserInterface } from "@/interfaces/UserInterface";
 
 export const chargeAndTopUpCoins = async (data: TopUpTransactionDataInterface) => {
     const configs = localStorage.getItem("accessToken") != undefined ? { headers: { 'Authorization' : `Bearer ${localStorage.getItem("accessToken")}`} } : {}
-    const axios_res = await axios.post(`${appConfig.BACKEND_URL}/topup/coins`, data, configs )
+    const axios_res = await axios.post(`${appConfig.BACKEND_URL}/api/topup/coins`, data, configs )
     const res = axios_res.data as ApiResponseInterface<UserInterface>
     if(!isHttpStatusOk(res.code)) throw new ApiErrorResponse(res.message ?? "", res.code, res.errors ?? undefined, res.tag ?? "")
     return res;
