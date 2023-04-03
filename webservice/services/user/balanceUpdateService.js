0b95b8a9bdc93c5ed98eba2d6a89ddbbefe7d379
bbompk <panithiv@gmail.com>
2023-04-03 20:51:52 +0700

    fix: change transfer coins to guide to 21.00

--- webservice/services/user/balanceUpdateService.js
+++ webservice/services/user/balanceUpdateService.js
@@ -1,23 +1,28 @@
 const cron = require('node-cron');
 const Program = require('../../models/Program');
 const User = require('../../models/User');
+const Notification = require('../../models/Notification');
 
 const StartUpdateUsersBalanceEveryMidnight = () => {
     console.log('Updating users balance every midnight');
-    cron.schedule('0 0 * * *', async () => {
+    cron.schedule('21 0 * * *', async () => {
         console.log('Updating users balance...')
         const programs = await Program.find({ status: 'ongoing' });
         programs.forEach(async (program) => {
             const now = new Date();
             const programEndDate = new Date(program.endDate);
             if (now > programEndDate) {
-                const users = await User.find({ program: program._id });
-                users.forEach(async (user) => {
-                    user.balance += program.price;
-                    await user.save();
-                });
+                await User.findByIdAndUpdate(program.guide, { $inc: { remainingAmount: program.price } });
                 program.status = 'complete';
                 await program.save();
+                const transfer_noti = new Notification({
+                    user: program.guide,
+                    type: 'transfer',
+                    title: 'Coins Received',
+                    message: `You have received ${program.price} Baht from ${program.name}`,
+                    isRead: false
+                });
+                await transfer_noti.save();
             }
         });
     });
