bed67d736cfc153b60960271b62424d43c950785
Bhuribhat <Bhuribhat@gmail.com>
2023-04-03 18:02:05 +0700

    style top up coin

--- frontend/components/topup/topup.tsx
+++ frontend/components/topup/topup.tsx
@@ -12,7 +12,7 @@ const topUpValues: TopUpValue[] = [
   { label: "500", value: 500 },
   { label: "700", value: 700 },
   { label: "1000", value: 1000 },
-  { label: "2000", value: 2000},
+  { label: "2000", value: 2000 },
   { label: "3000", value: 3000 },
 ];
 
@@ -39,23 +39,52 @@ const TopUp: React.FC<TopUpProps> = ({ initialAmount }) => {
 
   return (
     <div>
-      <h1>Coin Top Up</h1>
-      <div>Top up value (THB)</div>
+      <h1
+        style={{
+          alignItems: "center",
+          textAlign: "center",
+          margin: "auto",
+          alignSelf: "center",
+          paddingBottom: "20px",
+          paddingTop: "45px",
+          fontWeight: "bold",
+          // backgroundColor: COLOR.background,
+        }}
+      >
+        Coin Top Up
+      </h1>
+      <div
+        style={{
+          alignItems: "center",
+          padding: "10px 20px 10px 20px",
+          margin: "auto",
+          alignSelf: "center",
+        }}
+      >
+        Top up value (THB)
+      </div>
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(2, 1fr)",
           gap: "10px",
+          padding: "10px 20px 10px 20px",
         }}
       >
         {topUpValues.map(({ label, value }) => (
           <button
             key={value}
             style={{
+              fontSize: 17,
               padding: "10px",
-              border: "1px solid gray",
+              border: "1px solid lightgray",
               borderRadius: "5px",
-              backgroundColor: amount === value ? "lightblue" : "white",
+              backgroundColor: "white",
+
+              // backgroundColor: amount === value ? COLOR.orange : "white",
+              borderColor: amount === value ? COLOR.orange : "lightgray",
+              fontWeight: amount === value ? "bold" : "normal",
+              color: amount === value ? COLOR.orange : "black",
             }}
             onClick={() => handleValueClick(value)}
           >
@@ -63,18 +92,41 @@ const TopUp: React.FC<TopUpProps> = ({ initialAmount }) => {
           </button>
         ))}
       </div>
+      <div
+        style={{
+          alignItems: "center",
+          padding: "10px 20px 10px 20px",
+          margin: "auto",
+          alignSelf: "center",
+        }}
+      >
       Input Amount (THB) :
-       <input
+      </div>
+      <input
         type="number"
         value={amount}
         onChange={handleAmountChange}
         placeholder="Enter custom amount"
-        style={{ marginTop: "10px" }}
+        style={{ 
+          width: 350,
+          margin: "auto", 
+          color: COLOR.orange,
+          padding: "10px 10px 10px 10px", 
+          transform: "translateX(20px)",
+          border: "1px solid grey",
+          fontSize: 17,
+          fontWeight: "bold",
+        }}
       />
-      <div style={{ margin: "0.0rem", backgroundColor: COLOR.secondary }}>
+      <div style={{ 
+        margin: "0.0rem", 
+        padding: "10px 20px 10px 20px", 
+        // backgroundColor: COLOR.orange,
+      }}>
         {amount !== undefined && (
           <p style={{ marginTop: "10px" }}>
-            You have selected a top-up amount of THB {amount.toFixed(2)}
+            You have selected a top-up amount of 
+            THB {amount.toFixed(2)}
           </p>
         )}
       </div>
--- frontend/pages/topup/index.tsx
+++ frontend/pages/topup/index.tsx
@@ -5,12 +5,28 @@ import { chargeAndTopUpCoins } from "@/services/topupService";
 import NavBar from "@/components/layout/navBar";
 import { useState } from "react";
 import Link from "next/link";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 export default function Page() {
   return (
-    <div style={{ margin: "0.0rem", backgroundColor: COLOR.primary }}>
+    <div 
+      style={{ 
+        margin: "0.0rem", 
+        // backgroundColor: COLOR.primary 
+      }}
+    >
       <Link href={"./manage_account"}>
-        <button>Back</button>
+        <button
+          style={{
+            margin: ".3rem 0px 0px 0px",
+            background: "white",
+            border: "0px",
+            transform: "translate(2.3rem,5.5rem)",
+          }}
+          type="button"
+        >
+          <ChevronLeftIcon />
+        </button>
       </Link>
       <TopUp initialAmount={0} />
       {/* <NavBar /> */}
