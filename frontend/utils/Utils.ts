f78bc080cb4de89b08ae2bdb9968d7f6af0cbb7d
Pras Pitasawad <prasrodo@gmail.com>
2023-04-03 22:45:02 +0700

    use compare date function to check if program is in the pass

--- frontend/utils/Utils.ts
+++ frontend/utils/Utils.ts
@@ -11,3 +11,29 @@ export const addHoursToDate = (d: Date, h: number) => {
     d.setTime(d.getTime() + (h*60*60*1000));
     return d
 }
+
+export const isDateTimeInThePass = (d: Date, t: string) => {
+    const date = new Date(d);
+    const [hour,minute] = t.split(":")
+    const currentDate = new Date();
+    if(date.getFullYear() < currentDate.getFullYear()){
+        return true;
+    } else if (date.getFullYear() === currentDate.getFullYear()){
+        if(date.getMonth() < currentDate.getMonth()){
+            return true;
+        }else if (date.getMonth() === currentDate.getMonth()){
+            if(date.getDate() < currentDate.getDate()){
+                return true;
+            } else if (date.getDate()===currentDate.getDate()){
+                if(hour as unknown as number < currentDate.getHours()){
+                    return true;
+                } else if (hour as unknown as number === currentDate.getHours()){
+                    if(minute as unknown as number < currentDate.getMinutes()){
+                        return true;
+                    }
+                }
+            }
+        }
+    } 
+    return false;
+} 
